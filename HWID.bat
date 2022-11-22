a2b31eb3ad78a9b11c5c6b3496c9a7854c367ba8
Jay <20913004+jlaiii@users.noreply.github.com>
2022-11-21 17:33:56 -0800

    Add files via upload

--- HWID.bat
+++ HWID.bat
@@ -1,8 +1,20 @@
 @echo off
-systeminfo
+title HWID CHECKER
+color a
 echo.
+echo ==============HISTORY HWIDs===============================
+type C:\Users\%USERNAME%\AppData\Local\HWID.txt
+echo. >> C:\Users\%USERNAME%\AppData\Local\HWID.txt
+date/T >> C:\Users\%USERNAME%\AppData\Local\HWID.txt
+time/T >> C:\Users\%USERNAME%\AppData\Local\HWID.txt
+echo ===========================================================
+
+echo.
+echo ===============CURRENT=====================================
 wmic path win32_computersystemproduct get uuid
-echo.
-echo Motherboard
+wmic path win32_computersystemproduct get uuid >> C:\Users\%USERNAME%\AppData\Local\HWID.txt
 wmic baseboard get serialnumber
+wmic baseboard get serialnumber >> C:\Users\%USERNAME%\AppData\Local\HWID.txt
+echo =============================================================
+systeminfo | findstr /c:"BIOS Version"
 pause
